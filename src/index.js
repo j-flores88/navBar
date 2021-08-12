6049a6b3bab5348a2de37fb07c908cca25a857c6
Joel Flores <j_flores88@outlook.com>
2021-08-11 23:12:12 -0700

    Final working code for navBar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import navBarGenerator from "./navBarGenerator";
 
-navBarGenerator('NavBar', 'logo', 'Home', 'About', 'Contact')
+navBarGenerator('NavBar', 'logo', 'Home', 'About', 'Contact', 'Social');
+
+const showDropDown = () => {
+    const burger = document.querySelector('.burger');
+    const mobileMenu = document.querySelector('ul');
+
+    burger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('burgerDropDown')
+    });
+
+    window.addEventListener('resize', () => {
+        if(window.innerWidth > 786 && mobileMenu.classList.contains('burgerDropDown'))
+        mobileMenu.classList.remove('burgerDropDown')
+    })
+}
+
+showDropDown()
